b5d91f44d53df0b6d0418d469631552f71c946fa
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2025-06-03 20:03:24 +0200

    Made so you can't click button multiple times while it's adding the todo to database

--- src/app/_components/TodosForm.tsx
+++ src/app/_components/TodosForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
 import { createTodo } from "../_actions/Todos";
 import { useActionState } from "react";
 
@@ -10,15 +11,23 @@ function TodosForm() {
     <div>
       <form className="flex gap-2 items-center align-center" action={action}>
         <input className="text-black" type="text" id="title" name="title" />
-        <button
-          className="border rounded-md p-2 hover:bg-white hover:text-black transition-colors"
-          type="submit"
-        >
-          Add Todo
-        </button>
+        <SubmitButton />
       </form>
       {error?.title && <div className="text-red-500">{error.title}</div>}
     </div>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      className="border rounded-md p-2 hover:bg-white hover:text-black transition-colors"
+      type="submit"
+      disabled={pending}
+    >
+      {pending ? "Adding Todo..." : "Add Todo"}
+    </button>
   );
 }
 
